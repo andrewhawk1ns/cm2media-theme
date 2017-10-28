ee0378ecc0d68f46c3b04aaea441cd8d14d00b35
Andrew Hawkins <andrewhaekins@gmail.com>
2017-10-28 17:10:52 -0400

    Image and JS pipes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,40 +1,57 @@
 'use strict';
 
-
+import babel from 'gulp-babel';
 import gulp from 'gulp';
 import uglify from 'gulp-uglify';
-import browserSync from 'browser-sync';
+//import browsersync from 'browser-sync';
 import sourcemaps from 'gulp-sourcemaps';
 import rename from 'gulp-rename';
 import imagemin from 'gulp-imagemin';
 import merge from 'gulp-merge';
 import concat from 'gulp-concat';
-import autoprefixer from 'gulp-autoprefixer';
+//import autoprefixer from 'gulp-autoprefixer';
 
 const config = require("./package.json").config;
 
-gulp.task('frontend', () => {
-    return gulp.src('js/*.js')
+gulp.task('frontendJS', () => {
+    return gulp.src(config.assets.frontendScripts)
         .pipe(sourcemaps.init({'loadMaps': true}))
         .pipe(concat(config.public.frontendScripts))
-        .pipe(gulp.dest(config.public.dest))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest(config.public.destJS))
         .pipe(rename(config.public.frontendScriptsMin))
         .pipe(uglify())
         .pipe(sourcemaps.write('maps'))
-        .pipe(gulp.dest('public')), {base: './assets/'})
+        .pipe(gulp.dest(config.public.destJS))
+  });
+  
+  gulp.task('adminJS', () => {
+    return gulp.src(config.assets.adminScripts)
+    .pipe(sourcemaps.init({'loadMaps': true}))
+    .pipe(concat(config.public.adminScripts))
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(gulp.dest(config.public.destJS))
+    .pipe(rename(config.public.adminScriptsMin))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest(config.public.destJS))
   });
 
-  gulp.task('admin', () => {
-    return gulp.src('js/*.js')
-        .pipe(sourcemaps.init({'loadMaps': true}))
-        .pipe(concat(config.public.adminScripts))
-        .pipe(gulp.dest(config.public.dest))
-        .pipe(rename(config.public.adminScriptsMin))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('maps'))
-        .pipe(gulp.dest('public')), {base: './assets/'})
+  gulp.task( 'images', function() {
+    gulp.src( config.assets.images )
+      .pipe( imagemin( {
+            progressive: true,
+            optimizationLevel: 3,
+            interlaced: true,
+            svgoPlugins: [{removeViewBox: false}]
+          } ) )
+      .pipe(gulp.dest(config.public.destIMG))
   });
 
-gulp.task('build', ['frontend', 'admin']);
+gulp.task('build', ['images', 'frontendJS', 'adminJS']);
 
   
